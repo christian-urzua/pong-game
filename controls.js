3af8ca14d6b613e9a9110bcccffccf65fec14544
christian-urzua <chritianurzua124@gmail.com>
2024-07-16 16:16:54 -0400

    saving pong 1

--- controls.js
+++ controls.js
@@ -1,5 +1,40 @@
 window.addEventListener("keydown", keyDown);
 function keyDown(event) {
-    const key = event.key;
-    console.log(`KEYDOWN: ${key}`);
+    const key = event.code;
+    // console.log(`KEYDOWN: ${key}`);
+
+    switch (key) {
+        case "KeyW":
+            paddleL.vy = -paddleVelocity;
+            break;
+        case "KeyS":
+            paddleL.vy = paddleVelocity;
+            break;
+        case "ArrowUp":
+            paddleR.vy = -paddleVelocity;
+            break;
+        case "ArrowDown":
+            paddleR.vy = paddleVelocity;
+            break;
+        case "End":
+            resetGame();
+            break;
+    }
+}
+
+window.addEventListener("keyup", keyUp);
+function keyUp(event) {
+    const key = event.code;
+    // console.log(`KEYUP: ${key}`);
+
+    switch (key) {
+        case "KeyW":
+        case "KeyS":
+            paddleL.vy = 0;
+            break;
+        case "ArrowUp":
+        case "ArrowDown":
+            paddleR.vy = 0;
+            break;
+    }
 }
